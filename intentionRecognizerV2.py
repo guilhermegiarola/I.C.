287cc1fe904ae6d214445f01506ae2181cfd4d19
guilhermegiarola <guimapomollo@hotmail.com.br>
2019-09-29 12:21:48 -0300

    Add files via upload

--- intentionRecognizerV2.py
+++ intentionRecognizerV2.py
@@ -1,69 +1,47 @@
 import spacy
-
-counterArray = [0]*7
+import sys
 
 def intentionRecognizer(line):
-    nlp = spacy.load('pt')
+    nlp = spacy.load('pt_core_news_sm')
     processedLine = nlp(line)
 
     for token in processedLine:
-        if(token.lemma_ == 'apagar'):
-            counterArray[0] += 1
-            return
-
-        elif(token.lemma_ == 'acender'):
-            counterArray[1] += 1
-            return
-
-        elif(token.lemma_ == 'ligar'):
+        if(token.lemma_ in ['apagar','desligar','desativar']):
+            for token in processedLine:
+                if(token.lemma_ in ['lâmpada','luzir']):
+                    print("Apagou a luz.")
+                    return
+        elif(token.lemma_ in ['acender', 'acionar', 'ativar', 'ligar']):
+            for token in processedLine:
+                if(token.lemma_ in ['lâmpada', 'luzir']):
+                    print("Acendeu a luz.")
+                    return
+        elif(token.lemma_ in ['ligar', 'ativar', 'iniciar', 'acionar']):
             for token in processedLine:
                 if (token.lemma_ == 'ventilador'):
-                    counterArray[2] += 1
+                    print("Ligou o ventilador.")
                     return
-                elif (token.lemma_ == 'tv' or token.lemma_ == 'televisão'):
-                    counterArray[3] += 1
+                elif (token.lemma_ in ['televisão', 'tv', 'TV']):
+                    print("Ligou a televisão.")
                     return
-                elif (token.lemma_ == 'luz' or token.lemma_ == 'lâmpada'):
-                    counterArray[1] += 1
-                    return
-
-        elif(token.lemma_ == 'desligar'):
+        elif(token.lemma_ in ['desligar', 'desativar', 'terminar']):
             for token in processedLine:
                 if (token.lemma_ == 'ventilador'):
-                    counterArray[4] += 1
+                    print("Desligou o ventilador.")
                     return
-                elif(token.lemma_ == 'tv' or token.lemma_ == 'televisão'):
-                    counterArray[5] += 1
+                elif(token.lemma_ in ['televisão', 'tv', 'TV']):
+                    print("Desligou a televisão.")
                     return
-                elif(token.lemma == 'luz' or token.lemma == 'lâmpada'):
-                    counterArray[0] += 1
-                    return
-
-
-        elif(token.lemma_ == 'mudar'):
+        elif(token.lemma_ in ['mudar', 'trocar']):
             for token in processedLine:
-                if(token.lemma_ == 'canal'):
-                    counterArray[6] += 1
+                if(token.lemma_ in ['canal', 'emissora']):
+                    print("Trocou de canal.")
                     return
-
-def printResults():
-        print("Intenções: ")
-        print("     Apagar a luz: " + str(counterArray[0]))
-        print("     Acender a luz: " + str(counterArray[1]))
-        print("     Ligar o ventilador: " + str(counterArray[2]))
-        print("     Ligar a televisão: " + str(counterArray[3]))
-        print("     Desligar o ventilador: " + str(counterArray[4]))
-        print("     Desligar a televisão: " + str(counterArray[5]))
-        print("     Mudar de canal: " + str(counterArray[6]))
-        return
+        else:
+            print("Não entendi.")
+            return
 
 def main():
-    file = open("phraseList.txt", "r")
-
-    for line in file:
-        intentionRecognizer(line)
-
-    print()
-    printResults()
+    intentionRecognizer(sys.argv[1].lower())
 
 main()
